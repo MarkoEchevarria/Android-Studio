903a821d9126a8598d9f6ae982dd240af6c9532c
MarkoEchevarria <markofidel10@gmail.com>
2025-05-16 13:37:10 -0500

    Semana4.2

--- Semana3/3.BuildBeautifulApps/basic-android-kotlin-compose-training-superheroes-main/app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ Semana3/3.BuildBeautifulApps/basic-android-kotlin-compose-training-superheroes-main/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.superheroes
 
 import android.content.res.Configuration
@@ -68,12 +52,10 @@ fun HeroesList(
 ) {
     val visibleState = remember {
         MutableTransitionState(false).apply {
-            // Start the animation immediately.
             targetState = true
         }
     }
 
-    // Fade in entry animation for the entire list
     AnimatedVisibility(
         visibleState = visibleState,
         enter = fadeIn(
@@ -88,7 +70,6 @@ fun HeroesList(
                     hero = hero,
                     modifier = Modifier
                         .padding(horizontal = 16.dp, vertical = 8.dp)
-                        // Animate each list item to slide in vertically
                         .animateEnterExit(
                             enter = slideInVertically(
                                 animationSpec = spring(
@@ -168,10 +149,6 @@ fun HeroesPreview() {
         Surface (
             color = MaterialTheme.colorScheme.background
         ) {
-            /* Important: It is not a good practice to access data source directly from the UI.
-            In later units you will learn how to use ViewModel in such scenarios that takes the
-            data source as a dependency and exposes heroes.
-            */
             HeroesList(heroes = HeroesRepository.heroes)
         }
     }
